3b7d741bd5b5b10c1f0db4dc6662628fced48448
Francisco Gustavo <hidalgo_fco520@outlook.com>
2021-01-30 20:27:12 -0600

    improve: add file .env.local to set variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,13 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyC2mhJdxogyhPsookowwk2vKIHcAS3IIx8",
-    authDomain: "bogota-routes.firebaseapp.com",
-    databaseURL: "https://bogota-routes.firebaseio.com",
-    projectId: "bogota-routes",
-    storageBucket: "",
-    messagingSenderId: "384695102688"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 }
 
 const firebaseConf = firebase.initializeApp(config);
